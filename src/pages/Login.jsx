787d1ae551f901d44026e94a440186f6439cc98f
JackAutieri <jackautieri@gmail.com>
2024-07-17 14:10:55 -0400

    Add LoginForm Component and ajusted Login.jsx accordingly

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,63 +1,30 @@
-import React, { useState } from "react";
+import React from "react";
 import DocumentList from "../components/contexts/DocumentList";
-import { useUser } from "../components/contexts/UserContext"; // Provides access to user context and related actions.
+import LoginForm from "../components/contexts/LoginForm";
+import { useUser } from "../components/contexts/UserContext";
 
 const Login = () => {
-  // Manages state for user credentials and form inputs.
   const { user, handleLogin, handleLogout, error, documents } = useUser();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
 
-  // Handles user login on form submission.
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    try {
-      await handleLogin(email, password);
-      console.log("Login successful!");
-    } catch (error) {
-      console.error("Login failed:", error.message);
-    }
-  };
+  return (
+    <div>
+      <h2>Login</h2>
+      {error && <p className="error">{error}</p>}
+      {user ? (
+        <div className="user-info">
+          <p>Welcome, {user.email}</p>
+          <button onClick={handleLogout}>Logout</button>
 
-  // Main render function for the login component.
- // Main render function for the login component.
- return (
-  <div>
-    <h2>Login</h2>
-    {error && <p className="error">{error}</p>}
-    {user ? (
-      <div className="user-info">
-        <p>Welcome, {user.email}</p>
-        <button onClick={handleLogout}>Logout</button>
-
-        <div>
-          <h3>Documents:</h3>
-          <DocumentList documents={documents}/>
+          <div>
+            <h3>Documents:</h3>
+            <DocumentList documents={documents} />
+          </div>
         </div>
-      </div>
-    ) : (
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="email">Email:</label>
-        <input
-          type="email"
-          id="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-        <label htmlFor="password">Password:</label>
-        <input
-          type="password"
-          id="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-        <button type="submit">Login</button>
-      </form>
-    )}
-  </div>
-);
+      ) : (
+        <LoginForm handleLogin={handleLogin} /> // Use the LoginForm component here
+      )}
+    </div>
+  );
 };
 
 export default Login;
